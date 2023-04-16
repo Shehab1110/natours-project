cbea6a75202ad1e3bb510fe5cce8452504eb5a4c
Shehab Ashraf <shehabashrafh1110@gmail.com>
2023-04-16 15:53:55 +0200

    Reviews refactored

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -11,6 +12,7 @@ const reviewSchema = new mongoose.Schema(
       min: 1,
       max: 5,
       required: true,
+      set: (val) => Math.round(val * 10) / 10,
     },
     createdAt: {
       type: Date,
@@ -33,12 +35,53 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
     path: 'user',
     select: 'name -_id',
   });
   next();
+});
+
+reviewSchema.statics.calcRatingsAverage = async function (tourID) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourID },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourID, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourID, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
+};
+
+reviewSchema.post('save', function () {
+  this.constructor.calcRatingsAverage(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.findOne();
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  this.r.constructor.calcRatingsAverage(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
--- models/tourModel.js
+++ models/tourModel.js
@@ -118,6 +118,9 @@ const tourSchema = new mongoose.Schema(
   }
 );
 
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+
 // Virtual Field
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
