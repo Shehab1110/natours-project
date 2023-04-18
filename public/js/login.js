0f85479d5598c035a398b0c69e1f768ea5b61616
Shehab Ashraf <shehabashrafh1110@gmail.com>
2023-04-18 17:34:51 +0200

    minor changes

--- public/js/login.js
+++ public/js/login.js
@@ -18,16 +18,18 @@ const login = async (email, password) => {
         location.assign('/');
       }, 1500);
     }
+    console.log(res);
   } catch (err) {
+    console.log(err.response.data.message);
     showAlert('error', err.response.data.message);
   }
 };
 
-export const logout = async () => {
+const logout = async () => {
   try {
     const res = await axios({
       method: 'GET',
-      url: '/api/v1/users/logout',
+      url: 'http://127.0.0.1:3000/api/v1/users/logout',
     });
     if (res.data.status === 'success') location.reload(true);
   } catch (err) {
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const viewController = require('../controllers/viewController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
+
+// router.use(authController.isLoggedIn);
 
 router.get('/', viewController.getOverview);
 
